021c800557438a154f093dc4b943ffa17c307943
Zeke Sikelianos <zeke@sikelianos.com>
2024-09-10 16:45:35 -0700

    optionally read from eavesdrop transcript

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-## Environment Setup
+## Fresh Backgrounds
 
-1. Clone this repository.
-2. Run `npm install` to install dependencies.
-3. Copy `.env.example` to `.env`:
-   ```
-   cp .env.example .env
-   ```
-4. Edit `.env` and replace `your_replicate_api_token_here` with your actual Replicate API token.
+A thing that continuously generates fresh backgrounds for video conferencing.
 
-Make sure not to commit your `.env` file to version control.
+Sorry, this README isn't good yet.
 
--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 import 'dotenv/config'
 import Replicate from 'replicate'
 import download from 'download'
-import { copyFileSync } from 'node:fs'
+import { copyFileSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import dedent from 'dedent'
 
 // Initialize the Replicate client with your API token from .env
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN,
+  auth: process.env.REPLICATE_API_TOKEN
 })
 
-const theme = process.argv[2] || 'nature'
+let theme = process.argv[2]
 
 async function makePrompt () {
+  if (!theme) {
+    const transcriptPath = join(process.env.HOME, 'eavesdrop-transcript.txt')
+    const fileContent = readFileSync(transcriptPath, 'utf-8')
+    const lines = fileContent.trim().split('\n')
+    theme = lines.slice(-3).join('\n')
+  }
+
   const model = 'meta/meta-llama-3.1-405b-instruct'
   const input = {
-    prompt: `Something ${theme} themed`,
+    prompt: theme,
     system_prompt: dedent`
       You write descriptive and stylistic prompts for image generation models. 
       The prompts should generate images that look good with a human's face green-screened and centered on a layer above it. 
       You don't talk about the prompt. You just output it.
-    `,
-  };
+      The prompt you receive is a transcribed snippet of a recorded conversation. Select the topic from the conversation that is most identifiable as something that can be portrayed in an image, and use that as the subject.
+    `
+  }
 
-  console.log({input})
-  
+  console.log({ input })
+
   const output = await replicate.run(model, { input })
   return output.join('')
 }
 
-async function makeImage (prompt) {  
+async function makeImage (prompt) {
   const model = 'black-forest-labs/flux-schnell'
   const input = {
     prompt,
@@ -40,7 +48,7 @@ async function makeImage (prompt) {
 
   let predictionId
   let output
-  
+
   try {
     output = await replicate.run(model, { input }, ({ id }) => {
       predictionId = id
@@ -63,7 +71,7 @@ async function makeImage (prompt) {
 
 while (true) {
   const prompt = await makePrompt()
-  console.log({prompt})
+  console.log({ prompt })
   await makeImage(prompt)
   await new Promise(resolve => setTimeout(resolve, 1000))
 }
